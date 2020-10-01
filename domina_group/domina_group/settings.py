4af129a0c07be3df1d50766ee160d185c57e93ab
Martin Briceno <martin.briceno@live.com>
2020-10-01 15:28:52 -0400

    [settings ]add configuration and set database_url

--- domina_group/domina_group/settings.py
+++ domina_group/domina_group/settings.py
@@ -11,6 +11,35 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import psycopg2
+import dj_database_url
+import decouple inport config
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+# If the host name starts with 'live', DJANGO_HOST = "production"
+if socket.gethostname().startswith('live'):
+    DJANGO_HOST = "production"
+# Else if host name starts with 'test', set DJANGO_HOST = "test"
+elif socket.gethostname().startswith('test'):
+    DJANGO_HOST = "testing"
+else:
+# If host doesn't match, assume it's a development server, set DJANGO_HOST = "development"
+    DJANGO_HOST = "development"
+# Define general behavior variables for DJANGO_HOST and all others
+if DJANGO_HOST == "production":
+    DEBUG = False
+    STATIC_URL = 'https://yourappname.herokuapp.com'
+else:
+    DEBUG = True
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fk4*rx_3e=hgvq36-!xr%cjpk$e$jg8cd0j)k1f522@aq^u&od'
+SECRET_KEY = os.environ['SECRET_KEY'] or config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,7 +85,9 @@ ROOT_URLCONF = 'domina_group.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/www/STORE/main_app/templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,7 +105,7 @@ WSGI_APPLICATION = 'domina_group.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+#development
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -81,6 +113,8 @@ DATABASES = {
     }
 }
 
+#production
+DATABASES['default'] = dj_database_url.config(conn_max=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -118,11 +152,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 #email settings
 EMAIL_HOST = 'smtp.gmail.com'
@@ -132,3 +166,28 @@ EMAIL_HOST_PASSWORD = '77Pass$%'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_SSL = False
+
+# Define EMAIL_BACKEND variable for DJANGO_HOST
+if DJANGO_HOST == "production":
+    # Output to SMTP server on DJANGO_HOST production
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+elif DJANGO_HOST == "testing":
+    # Nullify output on DJANGO_HOST test
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+else:
+    # Output to console for all others
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Define CACHES variable for DJANGO_HOST production and all other hosts
+if DJANGO_HOST == "production":
+   # Set cache
+   CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+            'TIMEOUT':'1800',
+            }
+        }
+   CACHE_MIDDLEWARE_SECONDS = 1800
+else:
+   # No cache for all other hosts
+   pass
